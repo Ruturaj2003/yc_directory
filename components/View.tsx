c5f9bc4aba5189ee1411fed5456a8ef9b87ddbf2
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-13 11:35:27 +0530

    Add view tracking functionality, implement write client for updates, and fix environment variable handling

--- components/View.tsx
+++ components/View.tsx
@@ -1,20 +1,29 @@
 import { client } from '@/sanity/lib/client';
 import Ping from './Ping';
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
+import { writeClient } from '@/sanity/lib/write-client';
+import { after } from 'next/server';
 
 export const View = async ({ id }: { id: string }) => {
-  const viewsObj = await client
+  const { views: totalViews } = await client
     .withConfig({
       useCdn: false,
     })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
+  });
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping></Ping>
       </div>
       <p className="view-text">
-        <span className="font-black">{viewsObj.views} Views</span>
+        <span className="font-black">{totalViews} Views</span>
       </p>
     </div>
   );
